622060e39df379ce7feaef5dc34017033704668a
Kyle Lackinger <kbl40@georgetown.edu>
2022-08-05 12:10:09 -0400

    initial commit to new repo

---
+++ frontend/pages/_app.tsx
@@ -0,0 +1,19 @@
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import Layout from '../components/Layout'
+import TransactionToastProvider from '../providers/TransactionToastProvider'
+import { Toaster } from 'react-hot-toast'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <TransactionToastProvider>
+      <Toaster position="bottom-right" />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </TransactionToastProvider>
+    
+  )
+}
+
+export default MyApp
